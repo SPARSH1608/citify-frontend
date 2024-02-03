4f1d6066d20ed8bf5eb53fcd8f597a756c65be25
Sparsh Goel <sparshgeolk.com>
2024-02-03 22:08:36 +0530

    added sidebar changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 export default function layout() {
   return (
     <main>
       <Header />
-      <Outlet />
+      <div>
+        <Sidebar />
+        <Outlet />
+      </div>
     </main>
   );
 }
